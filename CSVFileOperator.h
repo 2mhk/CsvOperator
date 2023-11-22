667401253cb672daf2bb2109662569f38bd3aaa7
2mhk <907508272@qq.com>
2023-11-22 14:42:14 +0800

    Update CSVFileOperator.h

--- CSVFileOperator.h
+++ CSVFileOperator.h
@@ -1,39 +1,29 @@
-#pragma once
+#ifndef _CSVOPERATOR_H_
+#define _CSVOPERATOR_H_
 
-#include <fstream>
 #include <vector>
 #include <string>
-#include <map>
-#include <sstream>
-
-using namespace std;
 
 class CSVOperator
 {
-protected:
-	string m_content;
-	vector<vector<string>> m_result;
-	static int Error_line;		//not used at all
-	void clearresult();
+private:
+	CSVOperator() {}
+	~CSVOperator() {};
+    enum Status_splitprocess
+    {
+        SSP_NEW,
+        SSP_NORMAL,
+        SSP_INCONTENT,
+        SSP_OUTCONTENT
+    };
 
 public:
-	CSVOperator(std::string buffer);
-	CSVOperator() { m_content = ""; }
-	~CSVOperator();
-
-	enum Status_splitprocess
-	{
-		SSP_NEW,
-		SSP_NORMAL,
-		SSP_INCONTENT,
-		SSP_OUTCONTENT
-	};
-
-	int GetErrorLine() { return Error_line; };
-	vector<vector<string>> GetResult() { return m_result; }
-	vector<vector<string>> GetResult(std::string buffer);
-	static bool WriteResultToString(vector<vector<string>> _result, string &_outputstr);
+	static std::vector<std::vector<std::string>> GetResult(std::string buffer);
+	static bool WriteResultToString(std::vector<std::vector<std::string>> _result, std::string &_outputstr);
 	
 	//self tool
 	static std::string to_string_nozero(float input);
 };
+
+
+#endif 
